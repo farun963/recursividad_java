6cde02d8f1389e1cbf672144fbab126251f1a6fa
farun963 <gerar963@gmail.com>
2024-05-13 18:17:41 -0400

    Initial commit

---
+++ src/Fibonacci_iterativo.java
@@ -0,0 +1,26 @@
+public class Fibonacci_iterativo {
+
+    // Método para calcular el número de Fibonacci de manera iterativa
+    public static int fibonacciIterativo(int n) {
+        if (n <= 1)
+            return n;
+
+        int fib = 1;
+        int prevFib = 1;
+
+        for (int i = 2; i < n; i++) {
+            int temp = fib;
+            fib += prevFib;
+            prevFib = temp;
+        }
+        return fib;
+    }
+
+    // Método principal para probar la implementación iterativa
+    public static void main(String[] args) {
+        int n = 10; // El número de Fibonacci que queremos calcular
+        int resultado = fibonacciIterativo(n);
+        System.out.println("El número de Fibonacci en la posición " + n + " es: " + resultado);
+    }
+}
+
---
+++ src/Fibonacci_recursivo.java
@@ -0,0 +1,17 @@
+public class Fibonacci_recursivo {
+
+    // Método para calcular el número de Fibonacci de manera recursiva
+    public static int fibonacciRecursivo(int n) {
+        if (n <= 1)
+            return n;
+        return fibonacciRecursivo(n - 1) + fibonacciRecursivo(n - 2);
+    }
+
+    // Método principal para probar la implementación recursiva
+    public static void main(String[] args) {
+        int n = 10; // El número de Fibonacci que queremos calcular
+        int resultado = fibonacciRecursivo(n);
+        System.out.println("El número de Fibonacci en la posición " + n + " es: " + resultado);
+    }
+}
+
